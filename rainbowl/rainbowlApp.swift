f2a1c0c45ea07a55dd31e283f44bc823e9200626
hsiang2 <q224399377@gmail.com>
2023-04-29 10:42:28 +0800

    Initial Commit

---
+++ rainbowl/rainbowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  rainbowlApp.swift
+//  rainbowl
+//
+//  Created by 蔡相襄 on 2023/4/29.
+//
+
+import SwiftUI
+
+@main
+struct rainbowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
