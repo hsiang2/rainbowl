cf75c7060e30f48d16d0f75b0e56ded028a5ac94
hsiang2 <q224399377@gmail.com>
2024-02-02 23:29:00 +0800

    add sound

---
+++ rainbowl/View/Share/ShareView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareView.swift
+//  rainbowl
+//
+//  Created by 蔡相襄 on 2024/2/2.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareView()
+}
