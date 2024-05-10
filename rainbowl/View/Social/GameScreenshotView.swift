afd4c3c304dd62b927298ee962b0ffa5858ba87b
hsiang2 <q224399377@gmail.com>
2024-05-10 10:43:40 +0800

    Add share function

---
+++ rainbowl/View/Social/GameScreenshotView.swift
@@ -0,0 +1,18 @@
+//
+//  GameScreenshotView.swift
+//  rainbowl
+//
+//  Created by 蔡相襄 on 2024/5/4.
+//
+
+import SwiftUI
+
+struct GameScreenshotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameScreenshotView()
+}
